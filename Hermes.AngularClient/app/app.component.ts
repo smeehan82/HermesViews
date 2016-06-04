1da0b428be9dbaf2f769c0de33c5f45134f86e1d
Steven Meehan <smeehan82@gmail.com>
2016-06-04 11:19:59 -0400

    misc updates to base files

--- Hermes.AngularClient/app/app.component.ts
+++ Hermes.AngularClient/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { Routes, Router, ROUTER_DIRECTIVES } from '@angular/router';
 
 import DashboardComponent from './Dashboard/dashboard.component';
-import ContentTypeListComponent from './Content/content-type-list.component';
-import ContentTypeDetailsComponent from './Content/content-type-details.component';
-import TaxonomyListComponent from './Taxonomy/taxonomy-list.component';
-import TaxonomyDetailsComponent from './Taxonomy/taxonomy-details.component';
-import PageListComponent from './Page/page-list.component';
-import PageDetailsComponent from './Page/page-details.component';
-import UserListComponent from './Users/user-list.component';
-import UserDetailsComponent from './Users/user-details.component';
+import { ContentTypeComponent } from './Content';
+import TaxonomyComponent from './Taxonomy/taxonomy.component';
+import PageComponent from './Page/page.component';
+import UserComponent from './Users/user.component';
 import MyAccountComponent from './MyAccount/my-account.component';
 import SystemSettingsComponent from './SystemSettings/system-settings.component';
 
@@ -18,89 +14,31 @@ import SystemSettingsComponent from './SystemSettings/system-settings.component'
   template: `
     <h1>{{title}}</h1>
     <nav>
-      <a [routerLink]="['Dashboard']">Dashboard</a>
-      <a [routerLink]="['ContentTypes']">Content Types</a>
-      <a [routerLink]="['Taxonomy']">Taxonomy</a>
-      <a [routerLink]="['Pages']">Pages</a>
-      <a [routerLink]="['Users']">Users</a>
-      <a [routerLink]="['MyAccount']">My Account</a>
-      <a [routerLink]="['SystemSettings']">System Settings</a>
+      <a [routerLink]="['/']">Dashboard</a>
+      <a [routerLink]="['/content']">Content Types</a>
+      <a [routerLink]="['/taxonomy']">Taxonomy</a>
+      <a [routerLink]="['/pages']">Pages</a>
+      <a [routerLink]="['/users']">Users</a>
+      <a [routerLink]="['/my-account']">My Account</a>
+      <a [routerLink]="['/system-settings']">System Settings</a>
     </nav>
     <router-outlet></router-outlet>
   `,
   directives: [ROUTER_DIRECTIVES],
-  providers: [
-    ROUTER_PROVIDERS,
-  ]
 })
-@RouteConfig([
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: DashboardComponent,
-    useAsDefault: true
-  },
-  {
-    path: '/content-types',
-    name: 'ContentTypes',
-    component: ContentTypeListComponent,
-    useAsDefault: false
-  },
-  {
-    path: '/content-types/:id',
-    name: 'ContentTypeDetails',
-    component: ContentTypeDetailsComponent,
-    useAsDefault: false
-  },
-  {
-    path: '/taxonomy',
-    name: 'Taxonomy',
-    component: TaxonomyListComponent,
-    useAsDefault: false
-  },
-  {
-    path: '/taxonomy/:id',
-    name: 'TaxonomyDetails',
-    component: TaxonomyDetailsComponent,
-    useAsDefault: false
-  },
-  {
-    path: '/pages',
-    name: 'Pages',
-    component: PageListComponent,
-    useAsDefault: false
-  },
-  {
-    path: '/pages/:id',
-    name: 'PageDetails',
-    component: PageDetailsComponent,
-    useAsDefault: false
-  },
-  {
-    path: '/users',
-    name: 'Users',
-    component: UserListComponent,
-    useAsDefault: false
-  },
-  {
-    path: '/users/:id',
-    name: 'UserDetails',
-    component: UserDetailsComponent,
-    useAsDefault: false
-  },
-  {
-    path: '/my-account',
-    name: 'MyAccount',
-    component: MyAccountComponent,
-    useAsDefault: false
-  },
-  {
-    path: '/system-settings',
-    name: 'SystemSettings',
-    component: SystemSettingsComponent,
-    useAsDefault: false
-  },
+
+@Routes([
+  { path: '', component: DashboardComponent },
+  { path: '/content', component: ContentTypeComponent, },
+  { path: '/taxonomy', component: TaxonomyComponent, },
+  { path: '/pages', component: PageComponent, },
+  { path: '/users', component: UserComponent, },
+  { path: '/my-account', component: MyAccountComponent, },
+  { path: '/system-settings', component: SystemSettingsComponent, },
 ])
-export default class AppComponent {
+
+export default class AppComponent{
+  constructor(private router: Router) {}
+  
   title = 'Hermes Admin';
 }
--- Hermes.AngularClient/app/index.ts
+++ Hermes.AngularClient/app/index.ts
@@ -1,4 +1,6 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { ROUTER_PROVIDERS } from '@angular/router';
+import LOGGER_PROVIDERS from './Logging/Logger';
 
 declare var require: any;
 
@@ -8,4 +10,4 @@ require('zone.js/dist/zone');
 
 import AppComponent from './app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [ROUTER_PROVIDERS, LOGGER_PROVIDERS]);
